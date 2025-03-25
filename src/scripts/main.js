745641545f83f99089b00873db9e04ed6abf6e33
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2025-03-25 00:04:09 -0500

    Add functions to toggle modal

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,12 @@
+import { toggleModal } from "./utils.js";
 /**
  * Aquí estará la lógica principal de la aplicación.
  * Este bloque de código contiene la funcionalidad principal
  * que define el comportamiento del programa.
  */
+
+const closeBtn = document.querySelector("#modal .close");
+const searchMenu = document.querySelector("#search-menu");
+
+closeBtn.addEventListener("click", toggleModal);
+searchMenu.addEventListener("click", toggleModal);
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -3,3 +3,8 @@
  * Este archivo contiene funciones auxiliares que serán utilizadas y llamadas
  * desde el archivo principal para realizar varias operaciones.
  */
+
+export function toggleModal() {
+  const modal = document.querySelector("#modal");
+  modal.classList.toggle("hidden");
+}
